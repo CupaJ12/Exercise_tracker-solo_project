172e6b1b77f6954a4af6d9c4ddf3585010734e18
CupaJ12 <108900676+CupaJ12@users.noreply.github.com>
2023-02-28 11:23:58 -0500

    checklist completed in inputpage

--- src/components/InputPage/InputPage.jsx
+++ src/components/InputPage/InputPage.jsx
@@ -9,33 +9,57 @@ import { useState } from 'react';
 
 function InputPage() {
 	// declare constants, import reducers etc.
-    const dispatch = useDispatch();
-    
-    // let currentDate = new Date();
-    // console.log(currentDate);
-    // let date = currentDate.getDate();
-    const history = useHistory();
+	const dispatch = useDispatch();
+	// handle select for the checklist
+	const handleSelect = (event) => {
+		const value = event.target.value;
+		const isChecked = event.target.checked;
 
-    // const handleClick = () => {
-        // history.push('/LogHistory');
-    // }
-    const handleSubmitTwo = () => {
-        history.push('/LogHistory');
-    }
-    const handleSubmit = () => {
-        history.push('/CelebrationPage');
-        dispatch ({
-            type: 'INPUT_EXERCISE',
-            payload: {
-                // exercise1: exercise1,
-                // exercise2: exercise2,
-                // exercise3: exercise3,
-                // exercise4: exercise4,
-                // exercise5: exercise5,
-                // date: date
-            }
-        })
-    }
+		if (isChecked) {
+			//Add checked item into checkList
+			setCheckedList([...checkedList, value]);
+		} else {
+			//Remove unchecked item from checkList
+			const filteredList = checkedList.filter((item) => item !== value);
+			setCheckedList(filteredList);
+		}
+	};
+
+	const [checkedList, setCheckedList] = useState([]);
+	const listData = [
+		{ id: '1', value: 'exercise1' },
+		{ id: '2', value: 'exercise2' },
+		{ id: '3', value: 'exercise3' },
+		{ id: '4', value: 'exercise4' },
+		{ id: '5', value: 'exercise5' },
+		{ id: '6', value: 'exercise6' },
+	];
+
+	// let currentDate = new Date();
+	// console.log(currentDate);
+	// let date = currentDate.getDate();
+	const history = useHistory();
+
+	// const handleClick = () => {
+	// history.push('/LogHistory');
+	// }
+	const handleSubmitTwo = () => {
+		history.push('/LogHistory');
+	};
+	const handleSubmit = () => {
+		history.push('/CelebrationPage');
+		dispatch({
+			type: 'INPUT_EXERCISE',
+			payload: {
+				// exercise1: exercise1,
+				// exercise2: exercise2,
+				// exercise3: exercise3,
+				// exercise4: exercise4,
+				// exercise5: exercise5,
+				// date: date
+			},
+		});
+	};
 
 	return (
 		<main className='flex-container'>
@@ -54,15 +78,27 @@ function InputPage() {
 			</div>
 			<div className='flex-child inputs'>
 				<h2>Number Inputs</h2>
-                {/* new way of displaying exercises below: */}
-                {/* checklist below */}
-                
+				{/* new way of displaying exercises below: */}
+				{/* checklist below */}
+				{listData.map((item, index) => {
+					return (
+						<div key={item.id} className='checkbox-container'>
+							<input
+								type='checkbox'
+								name='exercises'
+								value={item.value}
+								onChange={handleSelect}
+							/>
+							<label>{item.value}</label>
+						</div>
+					);
+				})}
 
+			
+				
 
-
-
-				{/* old way of displaying exercises below: */}
-					{/* <label>Exercise 1</label>
+				;{/* old way of displaying exercises below: */}
+				{/* <label>Exercise 1</label>
 					<input type='number' className='exercise1' />
 					<label>Exercise 2</label>
 					<input type='number' className='exercise2' />
@@ -72,16 +108,11 @@ function InputPage() {
 					<input type='number' className='exercise4' />
 					<label>Exercise 5</label>
 					<input type='number' className='exercise5' /> */}
-
-
-
-
-
-
-					<label>Date</label>
-					<input type='date' className='date'  />
-				<button type="submit" onClick={() => handleSubmitTwo()} >Previous Logs</button>
-				
+				<label>Date</label>
+				<input type='date' className='date' />
+				<button type='submit' onClick={() => handleSubmitTwo()}>
+					Previous Logs
+				</button>
 			</div>
 			<div>
 				<h2>Date Input</h2>
